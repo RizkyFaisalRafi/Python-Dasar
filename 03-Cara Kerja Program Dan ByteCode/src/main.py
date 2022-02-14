64fd748e7410547eb97c9719f646fbea61763cbb
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-02-14 11:56:58 +0700

    Add files via upload

---
+++ 03-Cara Kerja Program Dan ByteCode/src/main.py
@@ -0,0 +1,13 @@
+""" Multiline Comment
+Python adalah Bahasa pemrograman yang interpreter langsung dijalankan 
+tanpa compile :
+Source Code -> Python / Interpreted -> Terminal / Dijalankan 
+"""
+print("Hallo Dunia")
+print("Aku Ganteng Sekalih")
+a = 10
+print(a)
+# Comment
+# Jika ingin bisa compile Python Ke yang namanya Byte Code agar program
+#  dijalankan lebih cepat pakai ini :
+# Source Code Byte Code -> Compiled -> Executable Terminal / Dijalankan
